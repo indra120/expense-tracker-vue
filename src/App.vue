b67fe942a5b9de7533f535af9416b5c0726d56d4
indra120 <indrapranata120@gmail.com>
2024-02-04 21:15:58 +0800

    Compute the income & expenses

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
   <div class="container">
     <Balance :total="total" />
-    <IncomeExpenses />
+    <IncomeExpenses :income="income" :expenses="expenses" />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
@@ -28,5 +28,19 @@ const total = computed(() => {
   return transactions.value.reduce((acc, transaction) => {
     return acc + transaction.amount
   }, 0)
+})
+
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2)
+})
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2)
 })
 </script>
